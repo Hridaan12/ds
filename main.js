db849f121878146bc57cd387c445e0dcb28dcc68
Hridaan12 <71926511+Hridaan12@users.noreply.github.com>
2021-06-23 23:29:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,53 +1,37 @@
-img = "";
-objects = [];
-status = "";
 
 function preload() {
-    img = loadImage('a..jpg');
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
 }
 
 function setup() {
-    canvas = createCanvas(380, 380);
-    canvas.center();
-   
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting Objects";
-}
+	canvas = createCanvas(1240, 336);
+	canvas.parent('canvas');
 
-function modelLoaded() {
-    console.log("Model Loaded!") 
-    status = true;
-  
-}
+	instializeInSetup(mario);
 
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 
-
-
-
-function gotResult(error, results) {
-    if (error) {
-        console.log(error);
-    }
-    console.log(results);
-    objects = results;
 }
 
 function draw() {
-    image(img, 0, 0, 380, 380);
-    if (status != "") {
-        r = random(255);
-        g = random(255);
-        b = random(255);
-        objectDetector.detect(img, gotResult);
-        
-        for (i = 0; i < objects.length; i++) {
-            document.getElementById("status").innerHTML = "Status : Object Detected";
-            document.getElementById("Number_Of_Objects").innerHTML = "Number of objects detected are : " + objects.length;
-           fill(r, g, b);
-           text(objects[i].label, objects[i].x, objects[i].y);
-           noFill();
-        stroke(r, g, b);
-        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-        }
-    }
+	game();
+
+}
+
+function modelLoaded() {
+	console.log('model Loaded !');
+}
+
+function gotPoses(results) {
+	if (results.length > 0) {
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
 }
